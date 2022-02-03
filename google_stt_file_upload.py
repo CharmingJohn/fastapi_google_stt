ecc5f357a7d86b1899afe14e9f5ea11e72083b7b
CharmingJohn <chaechunjae@gmail.com>
2022-02-03 22:41:18 +0900

    in_progress_google_stt_file_upload.py

--- google_stt_file_upload.py
+++ google_stt_file_upload.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 import logging
 import logging.handlers
 import re
@@ -18,13 +19,18 @@ api_key = '' # type your google stt api key
 api_url = 'https://speech.googleapis.com/v1/speech:recognize?alt=json&key=' + api_key
 
 @google_stt_app.post('/google_stt_file/')
-async def google_stt_api(config: dict, file):
+async def google_stt_api(config: dict, file: File):
+    UPLOAD_DIRECTORY = './'
+    contents = await file.read()
+
     config = {'encoding': config['encoding'],  # wav
               'sample_rate_hertz': config['sample_rate_hertz'],
               'language_code': config['language_code'],
               }
-
-    audio = {'content': base64.b64encode(file).decode('utf-8')}
+    if type(file) is not bytes: # in case, file is wav file
+        audio = {'content': base64.b64encode(file).decode('utf-8')}
+    else: # file is base64
+        audio = {'content': file.decode('utf-8')}
 
     payload = {
         'config': config,
@@ -41,5 +47,6 @@ async def google_stt_api(config: dict, file):
         for result in response['results']:
             result_string += result['alternatives'][0]['transcript']
 
+        return result_string
     except Exception as e:
-        print('error: ', e)
+        return e
